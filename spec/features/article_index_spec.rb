5cdc65423162aa02a9d368f64372cca37024816c
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-03-24 12:50:35 -0600

    Add features test for article index page

---
+++ spec/features/article_index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'article idex', type: :feature do
+  before(:each) do
+    @article_1 = Article.create!(title: "Week 1 at Turing", body: "Holy crap what happened.")
+    @article_2 = Article.create!(title: "Week 2 at Turing", body: "Drinking from a firehose.")
+  end
+  describe 'user sees all articles' do
+    it 'displays all articles' do
+      visit articles_path
+
+      expect(page).to have_content(@article_1.title)
+      expect(page).to have_content(@article_2.title)
+    end
+  end
+end
